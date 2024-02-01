6cd150b9feb844ff146e5d8a5c5b95da598e2e06
RuxandraRambet <ruxandraramb@gmail.com>
2024-02-01 08:23:11 +0100

    feat: added new images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,14 @@
         <div class="d-flex flex-wrap gap-3">
             @foreach ($trains as $train)
                 <div class="card" style="width: 25rem;">
-                    <img src="{{ Vite::asset('resources/img/trainimg.jpeg') }}" class="card-img-top" alt="">
+                    @if ($train['company'] === 'Frecciarossa')
+                        <img src="{{ Vite::asset('resources/img/frossa.png') }}" class="card-img-top pt-4"alt="Frecciarossa">
+                    @elseif ($train['company'] === 'Italo')
+                        <img src="{{ Vite::asset('resources/img/italo.png') }}" class="card-img-top"alt="Italo">
+                    @else
+                        <img src="{{ Vite::asset('resources/img/trenitalia.png') }}" class="card-img-top"alt="Trenitalia">
+                    @endif
+
                     <div class="card-body">
                         <p class="card-text"><strong>Azienda:</strong> {{ $train['company'] }}</p>
                         <p class="card-text"><strong>Stazione di partenza:</strong> {{ $train['departure_station'] }}</p>
@@ -24,13 +31,13 @@
                         <p class="card-text"><strong>Orario di arrivo:</strong> {{ $train['arrival_time'] }}</p>
                         <p class="card-text"><strong>Codice Treno:</strong> {{ $train['train_code'] }}</p>
                         <p class="card-text"><strong>Numero Carrozze:</strong> {{ $train['number_of_carriages'] }}</p>
-                        @if ( $train['is_on_time'] === 1 )
+                        @if ($train['is_on_time'] === 1)
                             <p class="card-text"><strong>In orario:</strong>Si</p>
                         @else
-                        <p class="card-text text-danger fw-bold">Treno cancellato</p>
+                            <p class="card-text text-danger fw-bold">Treno cancellato</p>
                         @endif
-                        
-                       
+
+
                     </div>
                 </div>
             @endforeach
